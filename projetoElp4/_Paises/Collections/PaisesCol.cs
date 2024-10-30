e0b678eeae4e136738c19df6c6955fc742a03a76
Ilann47 <ilanwendling@gmail.com>
2024-10-30 16:07:43 -0300

    Build(Paises): added Collections for paises

---
+++ projetoElp4/_Paises/Collections/PaisesCol.cs
@@ -0,0 +1,25 @@
+﻿using projetoElp4.MainWindow.Collection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetoElp4._Paises.Collections
+{
+    internal class PaisesCol : Colecoes<Paises>
+    {
+        public Paises PesquisarSigla(string sigla)
+        {
+            foreach(var oPais in aLista)
+            {
+                if (oPais.Sigla.Equals(sigla))
+                {
+                    return oPais;
+                }
+            }
+            return null;
+        }
+    }
+
+}
