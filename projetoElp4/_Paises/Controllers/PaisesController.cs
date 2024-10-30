d053894bc94e8c912840756ec2fdd8cf695383c2
Ilann47 <ilanwendling@gmail.com>
2024-10-30 16:08:07 -0300

    Build(Paises): added Controller for paises

---
+++ projetoElp4/_Paises/Controllers/PaisesController.cs
@@ -0,0 +1,40 @@
+﻿using projetoElp4._Paises.Daos;
+using projetoElp4.MainWindow.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetoElp4._Paises.Controllers
+{
+    internal class PaisesController : Controller
+    {
+        protected PaisDao oPaisDao;
+        public PaisesController() : base()
+        {
+            oPaisDao = new PaisDao();
+        }
+        public override string Salvar(Object obj)
+        {
+            return oPaisDao.Salvar(obj);
+        }
+
+        public override string Excluir(Object obj)
+        {
+            return oPaisDao.Excluir(obj);
+        }
+
+        public override Object CarregaTudo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Object CarregaObj(int IdObj)
+        {
+            throw new NotImplementedException();
+
+        }
+
+    }
+}
