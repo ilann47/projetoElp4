4bfc747a4cd2df1dc18e05bd62f3f791d7308d0d
Ilann47 <ilanwendling@gmail.com>
2024-10-30 16:00:21 -0300

    Build(MainWindow): added Dao

---
+++ projetoElp4/MainWindow/Dao/Dao.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetoElp4.MainWindow.Dao
+{
+
+    public abstract class Dao
+    {
+        public abstract string Salvar(Object obj);
+
+        public abstract string Excluir(Object obj);
+
+        public abstract Object CarregaTudo();
+
+        public abstract Object CarregaObj(int IdObj);
+
+    }
+}
