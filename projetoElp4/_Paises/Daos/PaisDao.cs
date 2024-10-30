e9b86a6415773669ef441c47841d744f50036253
Ilann47 <ilanwendling@gmail.com>
2024-10-30 16:08:25 -0300

    Build(paises): added Dao for Paises

---
+++ projetoElp4/_Paises/Daos/PaisDao.cs
@@ -0,0 +1,36 @@
+﻿using projetoElp4._Paises.Collections;
+using projetoElp4.MainWindow.Dao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetoElp4._Paises.Daos
+{
+    internal class PaisDao : Dao
+    {
+        public PaisDao() { }
+
+        public override string Salvar(object obj)
+        {
+            return " ";
+        }
+
+        public override string Excluir(object obj)
+        {
+            return " ";
+        }
+
+        public override Object CarregaTudo()
+        {
+            PaisesCol paisesCol = new PaisesCol();
+            return paisesCol;
+        }
+
+        public override Object CarregaObj(int IdObj)
+        {
+            return null;
+        }
+    }
+}
