16af72c1de68d54d1b6359bece2fefb722c271ac
Ilann47 <ilanwendling@gmail.com>
2024-10-30 15:08:49 -0300

    build(mainWindow): added main Controller

---
+++ projetoElp4/MainWindow/Controller/Controller.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetoElp4.MainWindow.Controller
+{
+    internal class Controller
+    {
+    }
+}
