a3e1f1f5f230fe49c0634f63485f9febc73e7e07
Ilann47 <ilanwendling@gmail.com>
2024-10-30 16:28:20 -0300

    Build(Cidades): Added Collection

---
+++ projetoElp4/_Cidades/Collection/CidadesCol.cs
@@ -0,0 +1,24 @@
+﻿using projetoElp4.MainWindow.Collection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetoElp4._Cidades.Collection
+{
+    internal class CidadesCol : Colecoes<Cidades>
+    {
+        public Cidades PesquisarSigla(string DDD)
+        {
+            foreach (var aCidade in aLista)
+            {
+                if (aCidade.Ddd.Equals(DDD))
+                {
+                    return aCidade;
+                }
+            }
+            return null;
+        }
+    }
+}
