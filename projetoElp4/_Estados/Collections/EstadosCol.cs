44741c934d031f4081c07ed45bcd2f6531f1a2d4
Ilann47 <ilanwendling@gmail.com>
2024-10-30 16:20:13 -0300

    Build(Estados): Added Colection

---
+++ projetoElp4/_Estados/Collections/EstadosCol.cs
@@ -0,0 +1,25 @@
+﻿using projetoElp4.MainWindow.Collection;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetoElp4._Estados.Collections
+{
+    internal class EstadosCol : Colecoes<Estados>
+    {
+        public Estados PesquisarSigla(string UF)
+        {
+            foreach (var oEstado in aLista)
+            {
+                if (oEstado.Uf.Equals(UF))
+                {
+                    return oEstado;
+                }
+            }
+            return null;
+        }
+    }
+}
